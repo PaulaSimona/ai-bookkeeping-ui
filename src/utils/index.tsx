cf7f57279679666d3fbc08c41dfd0e8dbebebfde
Diego Armando Zambrana Arze <contacto@diegozambrana.com>
2023-10-02 10:10:41 -0400

    function to control protocol from url

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,1 +1,12 @@
-export const API_DOMAIN: string = import.meta.env.VITE_API_DOMAIN;
+const protocol = document.location.protocol;
+let vite_api_domain: string = import.meta.env.VITE_API_DOMAIN;
+
+if (protocol == 'https:' && !vite_api_domain.includes('https:')) {
+  vite_api_domain = vite_api_domain.replace('http', 'https');
+}
+
+if (protocol == 'http:' && vite_api_domain.includes('https:')) {
+  vite_api_domain = vite_api_domain.replace('https', 'http');
+}
+
+export const API_DOMAIN: string = vite_api_domain;
