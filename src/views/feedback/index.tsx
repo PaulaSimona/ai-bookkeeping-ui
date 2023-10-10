8601c1137fdde9a38ac33f21b8b320b17f3fb73c
Diego Armando Zambrana Arze <contacto@diegozambrana.com>
2023-10-10 11:35:11 -0400

    create feedback template

--- src/views/feedback/index.tsx
+++ src/views/feedback/index.tsx
@@ -1,9 +1,41 @@
-import { FC } from 'react';
+import { BaseSyntheticEvent, FC, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import { Button } from '../../components/Button/Button';
 
 export const Feedback: FC = () => {
+  const [feedbackText, setFeedbackText] = useState('');
+
+  const onSubmitFeedback = (event: BaseSyntheticEvent) => {
+    event.preventDefault();
+    console.log('onSubmit', feedbackText);
+  };
   return (
-    <div>
-      <h1>Feedback</h1>
-    </div>
+    <Container>
+      <Row className="mb-2">
+        <Col>
+          <h2>Feedback</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Form onSubmit={onSubmitFeedback} style={{ maxWidth: 750 }}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Let us know how could improve</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="your opinion..."
+              rows={7}
+              value={feedbackText}
+              onChange={(e: BaseSyntheticEvent) =>
+                setFeedbackText(e.target.value)
+              }
+            />
+            <div className="mt-4" style={{ maxWidth: 200 }}>
+              <Form.Control as={Button} type="submit" value="Submit" />
+            </div>
+          </Form.Group>
+        </Form>
+      </Row>
+    </Container>
   );
 };
